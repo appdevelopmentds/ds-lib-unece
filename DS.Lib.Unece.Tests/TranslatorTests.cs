df4f98b3c5be2114a12adee29b602ccd0238cd07
appdevelopmentds <appdevelopment.ds@gmail.com>
2025-01-24 09:07:48 +0100

    UnitOfMeasureTranslator Switch to resx

---
+++ DS.Lib.Unece.Tests/TranslatorTests.cs
@@ -0,0 +1,129 @@
+using System.Diagnostics;
+using System.Globalization;
+
+namespace DS.Lib.Unece.Tests;
+
+[TestClass]
+public sealed class TranslatorTests
+{
+    private long memoryBefore = 0;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        // Garbage Collector ausführen, um Speichermessung genauer zu machen
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+        // Speicher vor der Ausführung messen
+        this.memoryBefore = GC.GetTotalMemory(forceFullCollection: true);
+    }
+
+    [TestCleanup]
+    public void CleanUp()
+    {
+        // Speicher nach der Ausführung messen
+        long memoryAfter = GC.GetTotalMemory(forceFullCollection: true);
+
+        // Arbeitsspeicherverbrauch berechnen
+        long memoryUsed = memoryAfter - memoryBefore;
+
+        // Ausgabe oder Testbedingung
+        Debug.WriteLine($"Memory Used: {memoryUsed / 1024.0 / 1024.0} MB");
+
+        // Assertion: Arbeitsspeicherverbrauch sollte kleiner als ein Grenzwert sein (z. B. 11 MB)
+        Assert.IsTrue(memoryUsed < 11_000_000);
+    }
+
+    [TestMethod]
+    public void Test_MTR_to_m()
+    {
+        string code = UnitOfMeasureTranslator.UNECEtoSI("MTR");
+        Assert.AreEqual("m", code);
+    }
+
+    [TestMethod]
+    public void Test_m_to_MTR()
+    {
+        string code = UnitOfMeasureTranslator.SItoUNECE("m");
+        Assert.AreEqual("MTR", code);
+    }
+    [TestMethod]
+    public void Test_LS()
+    {
+        string code = UnitOfMeasureTranslator.UNECEtoSI("LS");
+        Assert.AreEqual("lump sum", code);
+    }
+
+    [TestMethod]
+    public void Test_LS_de()
+    {
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo("de-DE");
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("de-DE");
+        string code = UnitOfMeasureTranslator.UNECEtoSI("LS");
+        Assert.AreEqual("Pauschal", code);
+    }
+
+    [TestMethod]
+    public void Test_MQW()
+    {
+        string code = UnitOfMeasureTranslator.UNECEtoSI("MQW");
+        Assert.AreEqual("m³·wk", code);
+    }
+    [TestMethod]
+    public void Test_UneceToSI_not_found()
+    {
+        var exception = Assert.ThrowsException<IndexOutOfRangeException>(() => { string siCode = UnitOfMeasureTranslator.UNECEtoSI("ABCD123"); });
+    }
+    [TestMethod]
+    public void Test_SIToUnece_not_found()
+    {
+        var exception = Assert.ThrowsException<IndexOutOfRangeException>(() => { string siCode = UnitOfMeasureTranslator.SItoUNECE("ABCD123"); });
+    }
+
+    [TestMethod]
+    public void Test_try_m_to_MTR()
+    {
+        bool success = UnitOfMeasureTranslator.TrySIToUNECE("m", out string code);
+        Assert.AreEqual("MTR", code);
+        Assert.IsTrue(success);
+    }
+
+    [TestMethod]
+    public void Test_try_MTR_to_m()
+    {
+        bool success = UnitOfMeasureTranslator.TryUNECEtoSI("MTR", out string code);
+        Assert.AreEqual("m", code);
+        Assert.IsTrue(success);
+    }
+
+    [TestMethod]
+    public void Test_try_UNCECE_not_found_without_fallback()
+    {
+        bool success = UnitOfMeasureTranslator.TryUNECEtoSI("BLAH", out string code);
+        Assert.IsFalse(success);
+        Assert.AreEqual("BLAH", code); // without fallback, returns the same.
+    }
+    [TestMethod]
+    public void Test_try_UNCECE_not_found_with_fallback()
+    {
+        bool success = UnitOfMeasureTranslator.TryUNECEtoSI("BLAH", out string code, "FB");
+        Assert.IsFalse(success);
+        Assert.AreEqual("FB", code); // fallback
+    }
+
+    [TestMethod]
+    public void Test_try_SI_not_found_without_fallback()
+    {
+        bool success = UnitOfMeasureTranslator.TrySIToUNECE("m4", out string code);
+        Assert.IsFalse(success);
+        Assert.AreEqual("m4", code); // without fallback, returns the same.
+    }
+    [TestMethod]
+    public void Test_try_SI_not_found_with_fallback()
+    {
+        bool success = UnitOfMeasureTranslator.TrySIToUNECE("m4", out string code, "FB");
+        Assert.IsFalse(success);
+        Assert.AreEqual("FB", code); // fallback
+    }
+}
--- DS.Lib.Unece/UnitOfMeasureTranslator.cs
+++ DS.Lib.Unece/UnitOfMeasureTranslator.cs
@@ -1,21 +1,28 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Resources;
+using System.Text;
 
 namespace DS.Lib.Unece
 {
-    public class UnitOfMeasureUneceSIConverter
+    public static class UnitOfMeasureTranslator
     {
-        private readonly static Rec20Rev17e __rec20_rev17e = new Rec20Rev17e();
+        private static readonly ResourceManager resourceManagerUNECESI = new ResourceManager("DS.Lib.Unece.ResRec20Rev17e-UNECESI", typeof(UnitOfMeasureTranslator).Assembly);
+        private static readonly ResourceManager resourceManagerSIUNECE = new ResourceManager("DS.Lib.Unece.ResRec20Rev17e-SIUNECE", typeof(UnitOfMeasureTranslator).Assembly);
 
         /// <summary>
-        /// Returns the SI-Unit-Symbol of the UNECEN-Code. 
+        /// Returns the SI-Unit-Symbol of the UNECE-Code. 
         /// Throws <see cref="IndexOutOfRangeException"/>, if not found.
         /// </summary>
         /// <param name="uneceCode"></param>
         /// <returns></returns>
         /// <exception cref="IndexOutOfRangeException"></exception>
-        public static string UneceToSI(string uneceCode)
+        public static string UNECEtoSI(string uneceCode)
         {
-            if (__rec20_rev17e.UneceToSIMap.TryGetValue(uneceCode, out string siCode))
+            string? siCode = resourceManagerUNECESI.GetString(uneceCode);
+
+            if (!String.IsNullOrEmpty(siCode))
             {
                 return siCode;
             }
@@ -34,35 +41,40 @@ namespace DS.Lib.Unece
         /// <param name="siCode">Output: the converted SI-symbol.</param>
         /// <param name="fallbackUneceCode">The fallback-value, if the symbol is not found. If not provided, the <paramref name="uneceCode"/> will be used.</param>
         /// <returns></returns>
-        public static bool TryUneceToSI(string uneceCode, out string siCode, string? fallbackSICode = null)
+        public static bool TryUNECEtoSI(string uneceCode, out string siCode, string? fallback = null)
         {
-            if (__rec20_rev17e.UneceToSIMap.TryGetValue(uneceCode, out siCode))
+            string? found = resourceManagerUNECESI.GetString(uneceCode);
+            if (!String.IsNullOrEmpty(found))
             {
+                siCode = found;
                 return true;
             }
             else
             {
-                siCode = fallbackSICode ?? uneceCode;
+                siCode = fallback ?? uneceCode;
                 return false;
             }
         }
 
+
         /// <summary>
-        /// Returns the UNECE-Unit of the SI-Symbol. 
+        /// Returns the UNECE-Code of the SI-Unit-Symbol. 
         /// Throws <see cref="IndexOutOfRangeException"/>, if not found.
         /// </summary>
         /// <param name="siCode"></param>
         /// <returns></returns>
         /// <exception cref="IndexOutOfRangeException"></exception>
-        public static string SIToUnece(string siCode)
+        public static string SItoUNECE(string siCode)
         {
-            if (__rec20_rev17e.SIToUneceMap.TryGetValue(siCode, out string uneceCodeCode))
+            string? uneceCode = resourceManagerSIUNECE.GetString(siCode);
+
+            if (!String.IsNullOrEmpty(uneceCode))
             {
-                return uneceCodeCode;
+                return uneceCode;
             }
             else
             {
-                throw new IndexOutOfRangeException($"SI-Code '{siCode}' not found.");
+                throw new IndexOutOfRangeException($"UNECE-Code '{siCode}' not found.");
             }
         }
 
@@ -73,19 +85,22 @@ namespace DS.Lib.Unece
         /// </summary>
         /// <param name="siCode">The SI-Symbol to find.</param>
         /// <param name="uneceCode">Output: the found UNECE-Unit.</param>
-        /// <param name="fallbackUneceCode">The fallback-value, if the symbol is not found. If not provided, the <paramref name="siCode"/> will be used.</param>
+        /// <param name="fallback">The fallback-value, if the symbol is not found. If not provided, the <paramref name="siCode"/> will be used.</param>
         /// <returns></returns>
-        public static bool TrySIToUnece(string siCode, out string uneceCode, string? fallbackUneceCode = null)
+        public static bool TrySIToUNECE(string siCode, out string uneceCode, string? fallback = null)
         {
-            if (__rec20_rev17e.SIToUneceMap.TryGetValue(siCode, out uneceCode))
+            string? found = resourceManagerSIUNECE.GetString(siCode);
+            if (!String.IsNullOrEmpty(found))
             {
+                uneceCode = found;
                 return true;
             }
             else
             {
-                uneceCode = fallbackUneceCode ?? siCode;
+                uneceCode = fallback ?? siCode;
                 return false;
             }
         }
+
     }
 }
